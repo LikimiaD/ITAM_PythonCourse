c21730577687b0ce42b29d11303ffc1e79186b9d
LikimiaD <somethingforsoftware@gmail.com>
2022-10-16 21:09:36 +0300

    16/10/2022

--- homework/homework_1_A.py
+++ homework/homework_1_A.py
@@ -1,5 +1,28 @@
-def greetings(name):
-    name = name.split()
-    print('Доброго времени суток, {} "Человек" {}'.format(name[0],name[1]))
+class Binary:
+    def __init__(self, num, value = 2):
+        self.num = num
+        self.value = value
+    def __add__(self, other):
+        return Binary(bin(int(self.num, self.value) + int(other.num, self.value))[2:])
+    def __sub__(self, other):
+        return Binary(bin(int(self.num, self.value) - int(other.num, self.value))[2:])
+    def __mul__(self, other):
+        return Binary(bin(int(self.num, self.value) * int(other.num, self.value))[2:])
+    def __floordiv__(self, other):
+        return Binary(bin(int(self.num, self.value) // int(other.num, self.value))[2:])
+    def __str__(self):
+        return str(int(self.num,self.value))
 
-greetings("Гендо Геннадий")
+
+w1 = Binary('101')
+w2 = Binary('10')
+add_ = w1+w2
+print(add_.num)
+sub_ = w1-w2
+print(sub_.num)
+mul_ = w1*w2
+print(mul_.num)
+floordiv_ = w1 // w2
+print(floordiv_.num)
+print(str(w1))
+print(str(w2))
--- homework/homework_1_B.py
+++ homework/homework_1_B.py
@@ -1,13 +1,36 @@
-def summation(start, end):
-    x = 0
-    if end > start:
-        for i in range(start, end+1):
-            x += i
-    else:
-        for i in range(end,start+1):
-            x += i
-    print(x)
+def transfer(value, n):
+        lst = []
+        while value >= n:
+            lst.append(value%n)
+            value //= n
+        lst.append(value)
+        lst = reversed(lst)
+        return ''.join(map(str, lst))
+class Ternary:
+    def __init__(self, num, value = 3):
+        self.num = num
+        self.value = value
+    def __add__(self, other):
+        return Ternary(transfer((int(self.num, self.value) + int(other.num, self.value)), self.value))
+    def __sub__(self, other):
+        return Ternary(transfer((int(self.num, self.value) - int(other.num, self.value)), self.value))
+    def __mul__(self, other):
+        return Ternary(transfer((int(self.num, self.value) * int(other.num, self.value)), self.value))
+    def __floordiv__(self, other):
+        return Ternary(transfer((int(self.num, self.value) // int(other.num, self.value)), self.value))
+    def __str__(self):
+        return str(int(self.num,self.value))
 
-summation(2,12)
-summation(-4,4)
-summation(3,2)
+
+w1 = Ternary('10')
+w2 = Ternary('2')
+add_ = w1+w2
+print(add_.num)
+sub_ = w1-w2
+print(sub_.num)
+mul_ = w1*w2
+print(mul_.num)
+floordiv_ = w1 // w2
+print(floordiv_.num)
+print(str(w1))
+print(str(w2))
